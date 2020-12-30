97ccaecd47b19d7d74919da883e74a60c4faaa97
Adan Escamilla <adanescamilla580@gmail.com>
2020-12-30 04:55:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,10 @@ require'pry'
 def key_for_min_value(name_hash)
 
 smallest_value = 0
- empty_hash = 0
+ empty_hash = nil
  
  name_hash.each do |name, value|
+   binding.pry
   if smallest_value == 0 || value < smallest_value
     smallest_value = value
     empty_hash = name
